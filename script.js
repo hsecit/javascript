fadde73824c67097c077a0f41f5dd6b13d825d8b
hamza <hamza_ezzourguane@um5.ac.ma>
2021-07-20 21:13:01 +0100

    js

--- script.js
+++ script.js
@@ -4,35 +4,51 @@ const queue = new QueuePriority()
 
 
 const priority = document.getElementById('priority')
-const task =  document.getElementById('task')
+const task = document.getElementById('task')
 const btn = document.getElementById('btn')
 
 const result = document.getElementById('res')
 
-const front =  document.getElementById('front')
+const front = document.getElementById('front')
 
-btn.addEventListener('click',()=> {
-    if ( priority.value !=='' && task.value!== 0 ) {
-        queue.enqueue(task.value,parseInt(priority.value))
-        console.log(queue.items);
-
-        if (queue.front()!== null) {
-            front.innerText = queue.front().toString()
-        }
- 
-        for (let i = 0; i < queue.items.length; i++) {
-            addElementToQueueUi(queue.items[i].toString())
-            
-        }
+btn.addEventListener('click', () => {
+    if (priority.value !== '' && task.value !== 0) {
+        queue.enqueue(task.value, parseInt(priority.value))
+        mountComponent()
     }
 })
 
+const mountComponent = () => {
+    showFront()
+    // delete the old elment 
+    updateUi()
+    // add fresh elemnt
+    for (let i = 0; i < queue.items.length; i++) {
+        addElementToQueueUi(queue.items[i].toString())
+
+    }
+}
+
+const showFront = () => {
+    if (queue.front() !== null) {
+        front.innerText = queue.front().toString()
+    }
+}
 
 
 
 const addElementToQueueUi = (el) => {
-    const element = document.createElement('div')
+    const element = document.createElement('p')
     element.innerHTML = el
     result.appendChild(element)
+}
+
+const updateUi = () => {
+    const Pcollection = document.getElementsByTagName('p')
+    for (let i = 0; i < Pcollection.length; i++) {
+        const element = Pcollection[i];
+        element.remove()
+
+    }
 }
 
